2375a2348780f02705d129ae5ad91970987e69cb
eaassuncao2 <eaassuncao@minha.fag.edu.br>
2022-08-29 19:27:35 -0300

    Atividade_01

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Eduardo Altemar Assunção
+          Eduardo Altemar Assunção.
         </p>
         <a
           className="App-link"
